8d874bc9f95ac72d610493be42398fc9c87254e3
paolaguerra <paolaguerraph@gmail.com>
2022-05-02 17:51:27 -0400

    Agregue state a mi componente TextBox

--- src/TextBox.js
+++ src/TextBox.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const TextBox = () => {
-  return (
-    <input className='textbox'></input>
-  )
-}
+  
+  const [myText, setmyText] = useState("");
+
+  const cambiosDelInput = (event) => {
+    setmyText(event.target.value);
+    console.log(myText);
+  };
+
+    return (
+      <div className="contenedor-notas">
+        <div className="contenedor-input">
+          <input
+            className="textbox"
+            type="text"
+            value={myText}
+            onChange={cambiosDelInput}
+          ></input>
+        </div>
+      </div>
+    )
+  };
+
