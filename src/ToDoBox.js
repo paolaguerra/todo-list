73c3116128032e3e3da3e598ae1635d5f4e3a5bf
paolaguerra <paolaguerraph@gmail.com>
2022-05-02 17:50:51 -0400

    Arreglos de estilo

--- src/ToDoBox.js
+++ src/ToDoBox.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export const ToDoBox = () => {
   return (
-    <div className='todo-box'></div>
-  )
-}
+    <div className="contenedor-todobox">
+      <div className="text-todo">
+        <div className="todo-box"></div>
+      </div>
+    </div>
+  );
+};
